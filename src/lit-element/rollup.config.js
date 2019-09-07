b05297256fba79a51da5e8b939bcedd8e097ce5d
Westbrook Johnson <westbrook.johnson@gmail.com>
2019-09-07 18:27:21 -0400

    User current lit-element and more equivilent rollup config.

--- src/lit-element/rollup.config.js
+++ src/lit-element/rollup.config.js
@@ -1,9 +1,13 @@
 import resolve from 'rollup-plugin-node-resolve';
+import { terser } from "rollup-plugin-terser";
+import minifyHTML from 'rollup-plugin-minify-html-literals';
 export default {
     entry: 'main.js',
     dest: 'bundle.js',
     format: 'iife',
     plugins: [
-        resolve()
+        minifyHTML(),
+        resolve(),
+        terser()
     ]
 };
